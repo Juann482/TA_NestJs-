9a3d1d99cd442e0a3ec13f383b76063315e4c6af
Ana <ana.acevedo2004@gmail.com>
2026-03-18 16:39:43 -0500

    Acceso_Ana

--- src/reg-acceso/controllers/reg-acceso/reg-acceso.controller.ts
+++ src/reg-acceso/controllers/reg-acceso/reg-acceso.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { RegAccesoService } from '../../services/reg-acceso/reg-acceso.service';
+import { CreateAccesoDto, UpdateAccesoDto } from '../../dtos/acceso.dto';
 
-@Controller('reg-acceso')
-export class RegAccesoController {}
+@Controller('accesos')
+export class RegAccesoController {
+    constructor(private service: RegAccesoService) {}
+
+    @Post()
+    create(@Body() dto: CreateAccesoDto) {
+        return this.service.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        return this.service.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.service.findOne(+id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateAccesoDto) {
+        return this.service.update(+id, dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.service.remove(+id);
+    }
+}
--- src/reg-acceso/services/reg-acceso/reg-acceso.service.ts
+++ src/reg-acceso/services/reg-acceso/reg-acceso.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Acceso } from '../../entities/acceso.entity';
+import { CreateAccesoDto, UpdateAccesoDto } from '../../dtos/acceso.dto';
 
 @Injectable()
-export class RegAccesoService {}
+export class RegAccesoService {
+    constructor(
+        @InjectRepository(Acceso)
+        private accesoRepo: Repository<Acceso>,
+    ) {}
+
+    // Crear
+    async create(dto: CreateAccesoDto) {
+        const acceso = this.accesoRepo.create({
+            usuarioId: dto.usuarioId,
+            observacion: dto.observacion,
+            horaIngreso: new Date()
+        });
+        return await this.accesoRepo.save(acceso);
+    }
+
+    // Obtener todos
+    async findAll() {
+        return await this.accesoRepo.find();
+    }
+
+    // Obtener uno
+    async findOne(id: number) {
+        return await this.accesoRepo.findOne({ where: { id } });
+    }
+
+    // Actualizar (registrar salida)
+    async update(id: number, dto: UpdateAccesoDto) {
+        await this.accesoRepo.update(id, {
+            horaSalida: dto.horaSalida || new Date(),
+            observacion: dto.observacion
+        });
+        return await this.findOne(id);
+    }
+
+    // Eliminar
+    async remove(id: number) {
+        return await this.accesoRepo.delete(id);
+    }
+}
